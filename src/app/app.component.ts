e264a6770dd0770b2a855fce4b5e707fd7dfe69f
Thiago <thiago2014ts@gmail.com>
2017-10-27 10:43:58 -0200

    Inclusão de estilização para componentes material; Menu lateral e conteúdo da página ajustados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../assets/css/material.css']
 })
 export class AppComponent {
   title = 'app';
